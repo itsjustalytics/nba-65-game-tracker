01aec7944dbfef7c556d0d6f16ab2e05c264ed28
itsjustalytics <itsjustalytics@gmail.com>
2026-03-29 00:02:02 -0500

    Add initial player tracker dataset and schema

---
+++ src/data/players.js
@@ -0,0 +1,92 @@
+export const players = [
+  {
+    id: 1,
+    name: "Shai Gilgeous-Alexander",
+    team: "OKC",
+    gamesPlayed: 68,
+    teamGamesPlayed: 72,
+    status: "Healthy",
+    injuryNote: null
+  },
+  {
+    id: 2,
+    name: "Nikola Jokic",
+    team: "DEN",
+    gamesPlayed: 66,
+    teamGamesPlayed: 72,
+    status: "Healthy",
+    injuryNote: null
+  },
+  {
+    id: 3,
+    name: "Giannis Antetokounmpo",
+    team: "MIL",
+    gamesPlayed: 60,
+    teamGamesPlayed: 72,
+    status: "Day-to-day",
+    injuryNote: "Knee soreness"
+  },
+  {
+    id: 4,
+    name: "Luka Doncic",
+    team: "DAL",
+    gamesPlayed: 58,
+    teamGamesPlayed: 72,
+    status: "Out",
+    injuryNote: "Hamstring strain"
+  },
+  {
+    id: 5,
+    name: "Joel Embiid",
+    team: "PHI",
+    gamesPlayed: 45,
+    teamGamesPlayed: 72,
+    status: "Out",
+    injuryNote: "Knee surgery"
+  },
+  {
+    id: 6,
+    name: "Jayson Tatum",
+    team: "BOS",
+    gamesPlayed: 70,
+    teamGamesPlayed: 72,
+    status: "Healthy",
+    injuryNote: null
+  },
+  {
+    id: 7,
+    name: "Anthony Edwards",
+    team: "MIN",
+    gamesPlayed: 64,
+    teamGamesPlayed: 72,
+    status: "Healthy",
+    injuryNote: null
+  },
+  {
+    id: 8,
+    name: "Devin Booker",
+    team: "PHX",
+    gamesPlayed: 62,
+    teamGamesPlayed: 72,
+    status: "Healthy",
+    injuryNote: null
+  },
+  {
+    id: 9,
+    name: "Kawhi Leonard",
+    team: "LAC",
+    gamesPlayed: 52,
+    teamGamesPlayed: 72,
+    status: "Out",
+    injuryNote: "Knee management"
+  },
+  {
+    id: 10,
+    name: "Cade Cunningham",
+    team: "DET",
+    gamesPlayed: 50,
+    teamGamesPlayed: 72,
+    status: "Out",
+    injuryNote: "Collapsed lung"
+  }
+];
